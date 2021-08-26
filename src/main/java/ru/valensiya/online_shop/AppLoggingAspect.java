c781d7e2252dc1fac6787623f4f5bd48aed8275b
Valentina <gvalya.25@gmail.com>
2021-08-26 20:32:28 +0300

    С помощью АОП посчитано по каждому сервису суммарное время, уходящее на выполнение методов этих сервисов. И по endpoint'у /statistic можно получить данную статистику клиенту

--- src/main/java/ru/valensiya/online_shop/AppLoggingAspect.java
+++ src/main/java/ru/valensiya/online_shop/AppLoggingAspect.java
@@ -6,19 +6,34 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 @Aspect
 @Component
 public class AppLoggingAspect {
+
+    private Map<String, Long> statisticMap;
+
+    @PostConstruct
+    private void init() {
+        this.statisticMap = new HashMap<>();
+    }
 
     @Around("execution(public * ru.valensiya.online_shop.services.*Service.*(..))")
     public Object methodProfiling(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("start profiling");
         long begin = System.currentTimeMillis();
         Object out = proceedingJoinPoint.proceed();
-        long end = System.currentTimeMillis();
-        long duration = end - begin;
-        System.out.println((MethodSignature) proceedingJoinPoint.getSignature() + " duration: " + duration);
-        System.out.println("end profiling");
+        long duration = System.currentTimeMillis() - begin;
+        String name = proceedingJoinPoint.getSignature().getDeclaringType().getSimpleName();
+        statisticMap.put(name, statisticMap.getOrDefault(name, 0L) + duration);
         return out;
+    }
+
+    public Map<String, Long> getStatistics() {
+        return statisticMap;
     }
 }
--- src/main/java/ru/valensiya/online_shop/controllers/StatisticController.java
+++ src/main/java/ru/valensiya/online_shop/controllers/StatisticController.java
@@ -4,16 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.valensiya.online_shop.AppLoggingAspect;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/statistic")
 public class StatisticController {
+    private final AppLoggingAspect aspect;
 
 
     @GetMapping
-    public String getStatistic() {
-        String statistic = "statistic";
-        return statistic;
+    public Map<String, Long> getStatistic() {
+        return aspect.getStatistics();
     }
 }
